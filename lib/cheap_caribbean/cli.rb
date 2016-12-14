193e64fb34332085778e8a0fe3e7c468967f2747
Mary Umoh <umohm12@gmail.com>
2016-12-14 03:30:28 +0000

    completed menu method in CLI

--- lib/cheap_caribbean/cli.rb
+++ lib/cheap_caribbean/cli.rb
@@ -8,9 +8,9 @@ class CheapCaribbean::CLI
 
 #the list_deals method will put the travel deals to the console for user to view in numericle order
     def list_deal
-      puts " "
+      puts ""
       puts "********** AFFORDABLE TRAVEL DEALS. BOOK NOW! **********"
-      puts " "
+      puts ""
       CheapCaribbean::Deal.each_with_index(1) do |deal, i|
         puts "#{i}. #{deal.name}"
     end
@@ -24,12 +24,17 @@ class CheapCaribbean::CLI
        puts "Enter 'list' to view the deals again or 'exit' to exit the program"
        puts ""
        input = gets.strip.downcase
-       if inputs == "list"
-         list_deal
-       if input.to_i < 0 && input.to_i < 8 # look for input between 1-7
+       if input.to_i > 0 && input.to_i < 8 # look for input between 1-7
          deal = CheapCaribbean::Deal.select_deal(input.to_i)
-
+         display_details(deal)
+       elsif input == "list"
+         list_deal
+       elsif input == "exit"
+         goodbye
+      else
+         puts "Please input a number between 1-7"
+      end
     end
-  end
+  end     #not sure how mNy ends I should have.
 
-  end
+end
