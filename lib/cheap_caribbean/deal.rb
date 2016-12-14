11338a3aafa32ea90d3d0e4b1cc174551ac25f6e
Mary Umoh <umohm12@gmail.com>
2016-12-14 16:59:29 +0000

    updating local/remote

--- lib/cheap_caribbean/deal.rb
+++ lib/cheap_caribbean/deal.rb
@@ -1,14 +1,14 @@
 class CheapCaribbean::Deal
-  attr_accessors :name, :location, :description, :url 
-  
-  @@all = []
-  
-  def initialize(name = " ", location = " ")
-    @name = name 
-    @location = location 
-    @description = description
-    @url = url 
-    scrape_details
-  
-  
+
+  attr_accessors :name, :location, :description, :url
+
+ @@all = []
+
+ def initialize(name = " ", location = " ")
+   @name = name
+   @location = location
+   @description = description
+   @url = url
+   scrape_details
+   
 end
