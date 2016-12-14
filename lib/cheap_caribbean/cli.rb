e181aba1a3e8d31994588a9a29b7513d0a611e90
Mary Umoh <umohm12@gmail.com>
2016-12-14 16:20:24 +0000

    updates to cli.rb

--- lib/cheap_caribbean/cli.rb
+++ lib/cheap_caribbean/cli.rb
@@ -17,7 +17,29 @@ class CheapCaribbean::CLI
   end
 
     def menu
-      
-    end
+      input = nil
+    while input != "exit"
+      puts ""
+      puts "Which deal would you like to view, pick a number?"
+      puts "Enter 'list' to view the deals again or 'exit' to exit the program"
+      puts ""
+      input = gets.strip.downcase
+      if input.to_i > 0 && input.to_i < 8 # look for input between 1-7
+        deal = CheapCaribbean::Deal.pick_deal(input.to_i)
+        display_details(deal)
+      elsif input == "list"
+        list_deal
+      elsif input == "exit"
+        goodbye
+     else
+        puts "Please input a number between 1-7"
+     end
+   end
+ end     #not sure how many ends I should have.
+
+   def goodbye
+     puts "Don't forget to come back for more deals!"
+   end
+ end
 
   end
