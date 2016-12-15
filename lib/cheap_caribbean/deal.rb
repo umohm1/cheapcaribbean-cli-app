12030f73bc7f74e7864455152d05d37b6783077a
Mary Umoh <umohm12@gmail.com>
2016-12-15 16:27:23 +0000

    added price to deal.rb

--- lib/cheap_caribbean/deal.rb
+++ lib/cheap_caribbean/deal.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CheapCaribbean::Deal
 
   attr_accessor :name, :location, :description, :price, :url
@@ -18,28 +17,5 @@ class CheapCaribbean::Deal
     binding.pry
     doc = Nokogiri::HTML(open('http://www.cheapcaribbean.com/'))
 
-
   end
 end
-=======
-  class CheapCaribbean::Deal
-
-    attr_accessors :name, :location, :description, :url
-
-   @@all = []
-
-   def initialize(name = " ", location = " ")
-     @name = name
-     @location = location
-     @description = description
-     @url = url
-     scrape_details
-   end
-
-   def self.scrape_cheap_caribbean
-     doc = Nokogiri::HTML(open('http://www.cheapcaribbean.com/'))
-
-   end
-
-   end
->>>>>>> 96eaabda6c00a75e1f577f15e7491445e15fe4fe
