1ed8e20cfc178296a3036e82f19e5a7126f947fb
Umohm1 <umohm12@gmail.com>
2016-12-16 21:34:40 +0000

    working on each_with_index

--- lib/cheap_caribbean/deal.rb
+++ lib/cheap_caribbean/deal.rb
@@ -6,7 +6,7 @@ end
 
 class CheapCaribbean::Deal
 
-  attr_accessor :name, :destination, :description, :price, :url
+  attr_accessor :name, :destination, :description, :price 
 
   @@all = []
 
@@ -15,24 +15,31 @@ class CheapCaribbean::Deal
     @destination = destination
     @description = description
     @price = price
-    @url = url
-    scrape_details
+  end
+
+  def to_s
+    puts "#{name} - #{destination} - #{description} - #{price}"
+    puts " "
   end
 
   def self.scrape
     content = open("http://www.cheapcaribbean.com/").read
     doc = Nokogiri::HTML(content)
     doc.css("div.content.oneColumn.mobileHomeDeal").each do |deal|
-      name = deal.css('h2').text.to_s.strip
-      destination = deal.css('a[gatrackitem=DestinationURL]').text.to_s.strip
+      new_deal = CheapCaribbean::Deal.new
+      new_deal.name = deal.css('h2').text.to_s.strip
+      new_deal.destination = deal.css('a[gatrackitem=DestinationURL]').text.to_s.strip
       description_link = deal.css('.mobileTallAdDesc>span.secondaryLink>a').attribute('href')
       description_url = "http://www.cheapcaribbean.com#{description_link}"
-      description = open(description_url).read.to_s.strip
-      price = deal.css('.estPrice.price.bold.taPriceDiv').text.to_s.strip
-      puts "#{name} - #{destination} - #{description} - #{price}"
-      puts " "
+      new_deal.description = open(description_url).read.to_s.strip
+      new_deal.price = deal.css('.estPrice.price.bold.taPriceDiv').text.to_s.strip
+      @@all << new_deal
     end
+    @@all
   end
 end
 
-CheapCaribbean::Deal::scrape
+deals = CheapCaribbean::Deal.scrape
+deals.each_with_index do |deal, index|
+  puts "#{index+1}. #{deal.name}" 
+end
