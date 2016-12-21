40af62471fb5b64726c53c22b51110e76374df3d
Umohm1 <umohm12@gmail.com>
2016-12-21 18:08:13 +0000

    updates

--- lib/cheap_caribbean/cli.rb
+++ lib/cheap_caribbean/cli.rb
@@ -14,27 +14,29 @@ class CheapCaribbean::CLI
     puts ""
     CheapCaribbean::Deal.all.each.with_index(1) do |deal, i|
       puts "#{i}. #{deal.name}"
-    end
+    end   
   end
+
 
   def menu
     input = nil
-    while input != "exit"
-      puts ""
-      puts "Which deal would you like to view, pick a number?"
-      puts "Enter 'list' to view the deals again or 'exit' to exit the program"
-      puts ""
-      input = gets.strip.downcase
+    while input != 'exit'
+    puts ""
+    puts "Which deal would you like to view, pick a number?"
+    puts "Enter 'list' to view the deals again or 'exit' to exit the program"
+    puts ""
+    input = gets.strip.downcase
       if input.to_i >= 1 && input.to_i <= 6 
-        deal = CheapCaribbean::Deal.pick_deal(input.to_i - 1)
+         deal = CheapCaribbean::Deal.pick_deal(input.to_i - 1)
         display_details(deal)
-      elsif input == "list"
+    elsif input == "list"
         list_deals
-      else 
+    else 
         puts "Please input a number between 1-6, exit or list"
       end
     end
   end
+
 
   def goodbye
     puts "Don't forget to come back for more deals!"
@@ -42,8 +44,11 @@ class CheapCaribbean::CLI
 
   def display_details(deal)
     puts "#{deal.name}" 
+    puts " "
     puts "#{deal.destination}" 
+    puts " "
     puts "#{deal.description}" 
-    puts "#{deal.price}" 
+    puts " "
+    puts "Price #{deal.price}" 
   end
 end
